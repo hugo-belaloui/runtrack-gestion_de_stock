9928f6252b201b1e9562b1aeab1f1ed4faaa70fe
hugo-belaloui <hugo.belaloui@laplateforme.io>
2026-03-06 14:58:53 +0100

    feat: add product insertion to Database class
    - Implemented `insert_product` method to handle adding new products to the database.
    - Added a debug print statement to verify all products.

--- Database.py
+++ Database.py
@@ -22,7 +22,12 @@ class Database:
         self._cursor.execute("SELECT * FROM product")
         products = self._cursor.fetchall()
         return products
-
+    def insert_product(self, id, name, desc, price, quant, id_cat):
+        request = "INSERT INTO product (id, name, description, price, quantity, id_category) VALUES (%s, %s, %s, %s, %s, %s)"
+        values = (id, name, desc, price, quant, id_cat)
+        self._cursor.execute(request, values)
+        self._db_connect.commit()
 
 db = Database()
+print(db.get_all_products())
 db.close_db()
